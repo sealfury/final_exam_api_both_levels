e682ae2838eee864326a1c8235ac9ceff2bd1a0d
sealfury <willspecker@gmail.com>
2021-02-03 13:08:14 +0100

    adds active model serializer gem and creates article/comment serializer

---
+++ spec/requests/api/article/comment/user_can_view_comments_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe 'GET /api/article/', type: :request do
+
+  describe '' do
+
+    before do
+
+      get '/endpoint/path',
+        params: {},
+        headers: {}
+    end
+    it 'is expected to...' do
+
+    end
+  end
+end
